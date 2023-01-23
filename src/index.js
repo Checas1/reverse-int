1a0a958572173736648ed0fa5c55b88a02a59978
checas1 <checas104@gmail.com>
2023-01-23 16:58:00 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  n = n >= 0? n : n * (-1)
+  let revers = n + ''
+  let smth = revers.split('')
+  let rever = smth.reverse() 
+  return Number(rever.join(''))
 }
